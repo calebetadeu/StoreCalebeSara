db61cb0de3b3092e96120152767593379faf1e7e
Calebe Tadeu <calebetadeu@gmail.com>
2021-10-29 21:43:39 -0300

    meta-tag

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -34,13 +34,13 @@ export default function Products({name,image,alt,price,buttonBuyNow}:ProductsPro
                       color='#561a86' 
                       height='3.5rem'
                        font_size='18px'
-                       link='https://api.whatsapp.com/send?phone=555598982677067&text=Quero%20comprar%20um%20presente%20pra%20voc%C3%AAs%20usando%20o%20Cart%C3%A3o%20de%20Cr%C3%A9dito'
+                       link={process.env.WATHSAPP_TALK as string}
                        /></div> 
                        <div className='button' ><Button name='Ajudar Com Outro Valor'
                         color='#2e3ca8'
                          height='3.5rem'
                           font_size='18px'
-                          link='https://nubank.com.br/pagar/5nbfl/GUZLEPo5jA'
+                          link={process.env.NUBANK_ACCOUNT as string}
                           /></div> 
                     </section>
                     
